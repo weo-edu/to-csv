725dbf60f1a3ac767268f276ee1a1d2cb3bd1d2c
Andrew Shaffer <andrew@weo.io>
2017-01-19 16:29:28 -0800

    escape cells properly

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 module.exports = function(rows) {
   return rows.map(function(row) {
-    return row ? row.join(',') : ''
+    return row ? row.map(escapeStr).join(',') : ''
   }).join('\n')
+}
+
+function escapeStr (str) {
+  return '"' + str.replace('"', '""') + '"'
 }
